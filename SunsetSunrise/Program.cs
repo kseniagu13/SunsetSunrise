c042f8638997738a0e0e2eda4767feaf9ff86fa8
Ksenia Gustsenko <kseniagustsenko@Ksenias-MacBook-Air.local>
2020-01-17 14:57:31 +0200

    SunsetSunrise

---
+++ SunsetSunrise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SunsetSunrise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string responseUrl = "https://api.sunrise-sunset.org/json?lat=59.437222&lng=24.753574&date=today";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(responseUrl);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                SunMode sunMode = JsonConvert.DeserializeObject<SunMode>(response); //loome objekti, et salvestada sinna andmed jsonist
+
+                Console.WriteLine(sunMode.Status);
+                Console.WriteLine($"Sunrise: {sunMode.Results.Sunrise}"); 
+                Console.WriteLine($"Sunrise: {sunMode.Results.Sunset}");
+            }
+        }
+    }
+}
---
+++ SunsetSunrise/SunMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SunsetSunrise
+{
+   class SunMode
+    {
+        public string Status { get; set; } // omadus, peab oelma sama nimega mis seal url-s ja suure S sest getter ja setter on vajalikud
+        public Results Results { get; set; } //omadus, seal sees on teised omaduses
+    }
+
+    public class Results
+    {
+        public string Sunrise { get; set; } //omadus, ta on Resultsi sees
+        public string Sunset { get; set; }//omadus
+    }
+}
